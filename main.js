4c366e5d4adc19ab2090c6feefbcf031959ca7f9
Katerina Savina <katerina.savina@yahoo.com>
2018-04-01 21:47:38 +0300

    timeline + diagram

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $('#circle1').click(function() {
     $(this).css({'background' : '#808080'}).parent().parent().find(".timeline__step--third, .timeline__step--second").find(".timeline__step-line, .timeline__step-circle").css({ 'background' : '#dfdfdf'});
 });
 $('#circle2').click(function() {
-    $(this).css({'background' : '#808080'}).parent().find(".timeline__step-line").css({'background' : '#808080'}).parent().parent().find(".timeline__step--third").find(".timeline__step-line, .timeline__step-circle").css({ 'background' : '#dfdfdf'});
+    $(this).css({'background' : '#808080'}).parent().find(".timeline__step-line, .timeline__step--first").css({'background' : '#808080'}).parent().parent().find(".timeline__step--third").find(".timeline__step-line, .timeline__step-circle").css({ 'background' : '#dfdfdf'});
 });
 $('#circle3').click(function() {
     $(this).css({'background' : '#808080'})
