1f2b55d251232fe1f82a41b9548657754f9b93f0
Katerina Savina <katerina.savina@yahoo.com>
2018-04-01 20:14:35 +0300

    Initial

---
+++ main.js
@@ -0,0 +1,11 @@
+$('#circle1').click(function() {
+    $(this).css({'background' : '#808080'}).parent().parent().find(".timeline__step--third, .timeline__step--second").find(".timeline__step-line, .timeline__step-circle").css({ 'background' : '#dfdfdf'});
+});
+$('#circle2').click(function() {
+    $(this).css({'background' : '#808080'}).parent().find(".timeline__step-line").css({'background' : '#808080'}).parent().parent().find(".timeline__step--third").find(".timeline__step-line, .timeline__step-circle").css({ 'background' : '#dfdfdf'});
+});
+$('#circle3').click(function() {
+    $(this).css({'background' : '#808080'})
+        .parent().parent().find(".timeline__step-line, .timeline__step-circle").css({'background' : '#808080'});
+});
+
